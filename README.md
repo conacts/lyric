4d84fbb4977b089d4259ee46ed2f069be4f39328
cs <csheehan630@gmail.com>
2023-02-09 13:58:35 -0500

    adding lyric data

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # lyric
+This is an implementation of a natural language processing (NLP) algorithm that uses Drake lyrics as its training data. The purpose of this algorithm is to generate new, original lyrics that are in the style of Drake.
+
+Data Collection
+The training data for this algorithm consists of lyrics from Drake's songs, which were obtained through using the web scraping library Selenium. The data was pre-processed to remove any extraneous information and ensure that only the lyrics remained.
+
+Model Architecture
+Lyric is based on the deep learning architectureNetwork (RNN) or a Transformer. The model is trained on the collected lyrics data, learning to predict the next word in a given sequence of lyrics based on the context of the words that came before it.
+
+Evaluation and Deployment
+The performance of the algorithm can be evaluated using metrics such as accuracy and perplexity. Once the model has been trained and evaluated, it can be deployed for use by music artists and songwriters who are looking to generate new lyrics in the style of Drake. The algorithm can be run in real-time, providing users with instant results.
+
+Future Work
---
+++ getlyrics.py
@@ -0,0 +1,36 @@
+import re
+import random
+import time
+from bs4 import BeautifulSoup
+from lxml import etree
+import requests
+
+
+songs = []
+HEADERS = {
+        'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47',
+        'accept-language' : 'en-US,en;q=0.9,it;q=0.8,es;q=0.7',
+        'accept-encoding': 'gzip, deflate, br',
+        'Referer': 'https://www.google.com/',
+        }
+
+with open('data/songlist.txt') as f:
+    lines = f.readlines() 
+    for line in lines:
+        songs.append(re.sub('[^A-Za-z0-9]+', '', line).lower())
+
+with open('data/drakelyrics.txt', 'a') as f:
+    for i, song in enumerate(songs):
+        url = 'https://www.azlyrics.com/lyrics/drake/' + song + '.html'
+        page = requests.get(url, headers=HEADERS).text
+        soup = BeautifulSoup(page, 'html.parser')
+        dom = etree.HTML(str(soup))
+        lyrics = dom.xpath('/html/body/div[2]/div[2]/div[2]/div[5]/text()')
+        time.sleep(random.randint(4, 8))
+        if len(lyrics) > 0:
+            print('[', i+1, ":", song, ']', lyrics[0])
+            for i in range(len(lyrics)):
+                lyric = dom.xpath('/html/body/div[2]/div[2]/div[2]/div[5]/text()')[i]
+                f.write(lyric)
+        else:
+            print('[', i+1, ':', song, ':',  'SCRAPING FAILED ]')
